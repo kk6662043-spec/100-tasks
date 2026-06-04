b5674b5c4b8b228114edf7fd32cac97de4e6436f
kk6662043-spec <kk6662043@gmail.com>
2026-06-04 19:34:45 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,8 @@
+name = input("Name: ")
+email = input("Email: ")
+message = input("Message: ")
+
+print("\nContact Form Details")
+print("Name:", name)
+print("Email:", email)
+print("Message:", message)
