34ae64927cbc1c9719f98dd6dfa6965ad1aafa71
kk6662043-spec <kk6662043@gmail.com>
2026-06-05 11:40:07 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,10 @@
+import random
+
+secret = random.randint(1, 10)
+
+guess = int(input("Guess a number (1 to 10): "))
+
+if guess == secret:
+    print("Correct! You win 🎉")
+else:
+    print("Wrong! The number was", secret)
